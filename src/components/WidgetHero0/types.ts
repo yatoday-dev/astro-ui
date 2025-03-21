473d772c2e52de2a705a9923f26c5548fee00b4d
alex <qwert2001qwert@gmail.com>
2025-03-21 21:03:12 +0100

    feat: Add WidgetHero0 component and corresponding documentation

---
+++ src/components/WidgetHero0/types.ts
@@ -0,0 +1,14 @@
+import type { WidgetFeatures0Props } from '~/components/WidgetFeatures0/types.ts';
+import type { ImageProps } from '../../utils/images-optimization.ts';
+import type { Link, Stat, ToAction } from '../../types';
+
+export type WidgetHero0Props = {
+  links?: Link[];
+  stats?: Stat[];
+  actions?: ToAction[];
+  title?: string;
+  description?: string;
+  tagline?: string;
+  image?: ImageProps | string;
+  position?: 'center' | 'left' | 'right';
+} & WidgetFeatures0Props;
