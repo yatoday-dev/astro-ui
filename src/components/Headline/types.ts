d245c9e7a33d7f718a916493ac01220e745fd4a9
alex <qwert2001qwert@gmail.com>
2025-03-22 13:05:42 +0100

    feat: Added WidgetHero1 component

--- src/components/Headline/types.ts
+++ src/components/Headline/types.ts
@@ -1,6 +1,9 @@
+import type {HTMLTag} from "astro/types";
+
 export type HeadlineProps = {
   title?: string;
   subtitle?: string;
   tagline?: string;
   classes?: Record<string, string>;
+  ['as']?: HTMLTag;
 };
---
+++ src/components/WidgetHero1/WidgetHero1.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+  import type { WidgetHero1Props } from './types'
+</script>
