17576fca51d455ea9e785682b440d52c716595a4
alex <qwert2001qwert@gmail.com>
2025-03-24 19:36:02 +0100

    feat: Add WidgetSteps1 component with documentation and examples

---
+++ src/components/WidgetSteps1/WidgetSteps1.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+  import type { WidgetSteps1Props } from './types'
+</script>
---
+++ src/components/WidgetSteps1/types.ts
@@ -0,0 +1,15 @@
+import type {Link, Stat, ToAction} from "~/types";
+import type {WidgetFeatures0Props} from "~/components/WidgetFeatures0/types.ts";
+import type {HeadlineProps} from "~/components/Headline/types.ts";
+
+export type WidgetSteps1Props = {
+  callToAction?: ToAction;
+  isReversed?: boolean;
+  items?: {
+    title?: string;
+    description?: string;
+    icon?: string;
+    href?: string;
+    classes?: Record<string, string>;
+  }[];
+} & WidgetFeatures0Props & HeadlineProps;
