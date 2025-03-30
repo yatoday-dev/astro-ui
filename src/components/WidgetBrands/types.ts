a50d81a67eb425854447d419b74e061aee377473
alex <qwert2001qwert@gmail.com>
2025-03-30 12:09:00 +0200

    Add WidgetBrands component and documentation

---
+++ src/components/WidgetBrands/types.ts
@@ -0,0 +1,8 @@
+import type { WidgetWrapperProps } from '../WidgetWrapper/types';
+import type { HeadlineProps } from '../Headline/types';
+import type {Image} from "~/types";
+
+export type WidgetBrandsProps = {
+  icons?: Array<string>;
+  images?: Array<Image>;
+} & WidgetWrapperProps & Omit<HeadlineProps, 'classes'>;
