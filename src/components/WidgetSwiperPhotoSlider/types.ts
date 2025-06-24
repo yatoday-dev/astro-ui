25d202fe7d8b8c6de7c1f4ab6e3aee1c156facca
Alex Kato <dev@mg.yatoday.es>
2025-06-24 13:47:43 +0200

    feat: add callToAction support in WidgetSwiperPhotoSlider

--- src/components/WidgetSwiperPhotoSlider/types.ts
+++ src/components/WidgetSwiperPhotoSlider/types.ts
@@ -1,10 +1,12 @@
 import type { WidgetWrapperProps } from '../WidgetWrapper/types';
 import type { HeadlineProps } from '../Headline/types';
+import type {ToAction} from "../../types";
 
 export type WidgetSwiperPhotoSliderProps = {
   isAfterContent?: boolean;
   withNavigation?: boolean;
   imagesFolder: string;
   classes?: Record<string, string>;
+  callToAction?: ToAction;
 } & WidgetWrapperProps &
   Omit<HeadlineProps, 'classes'>;
