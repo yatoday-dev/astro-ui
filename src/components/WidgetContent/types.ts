b3216cc147e8c6c2712f7ddd2145feeb97db85f1
Alex Kato <dev@mg.yatoday.es>
2025-07-11 21:59:51 +0200

    feat: enhance WidgetContent with interactive PointMap and update related components
    - Introduced `PointMap` integration in `WidgetContent` to support interactive points with `pointsDisplayMode`.
    - Refined class structure in `HeroSection` and `WidgetWrapper`.
    - Improved doc structure for clarity and added new `props` references.

--- src/components/WidgetContent/types.ts
+++ src/components/WidgetContent/types.ts
@@ -1,4 +1,4 @@
-import type { Item, ToAction } from '../../types';
+import type {Item, HotspotPoint, ToAction} from '../../types';
 import type { WidgetWrapperProps } from '../WidgetWrapper/types';
 import type { HeadlineProps } from '../Headline/types.ts';
 
@@ -7,6 +7,8 @@ export type WidgetContentProps = {
   defaultIcon?: string;
   image?: string | unknown;
   items?: Array<Item>;
+  points?: Array<HotspotPoint>;
+  pointsDisplayMode?: 'always' | 'hover' | undefined; // controls visibility of hotspot points
   columns?: number;
   isReversed?: boolean;
   isAfterContent?: boolean;
